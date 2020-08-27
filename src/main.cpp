38aef452728f2901cdf6ec4388747c88b0004d15
zegolem <zegolem1383@outlook.fr>
2020-08-27 20:44:48 +0200

    Fixed the main function's signature

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@
 #include "FramedWindow.h"
 #include "WindowManager.h"
 
-int main(void)
+int main(int argc, const char** argv)
 {
 	fprintf(stdout, "Hello from zwm!\n");
 
 	if (ZWM::WindowManager::the()->init() != 0)
 		return -1;
 	ZWM::WindowManager::the()->run_loop();
+	return 0;
 }
