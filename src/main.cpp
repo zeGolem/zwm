cff80ac4ac5b9d4ef1e3bb121422e65619b227a6
zegolem <zegolem1383@outlook.fr>
2020-12-03 20:53:14 +0100

    Added custom Error handler

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,21 @@
 #include "FramedWindow.h"
 #include "WindowManager.h"
 
-int main(int argc, const char** argv)
+int x_error_handler(Display *d, XErrorEvent * e)
+{
+	fprintf(stderr, "X ERROR: %x - %x\n", e->error_code, e->minor_code);
+	fprintf(stderr, "\ttype: 0x%x\n", e->type);
+	fprintf(stderr, "\tserial: 0x%lx\n", e->serial);
+	fprintf(stderr, "\tresourceid: 0x%lx\n", e->resourceid);
+	fprintf(stderr, "\trequest_code: 0x%x\n", e->request_code);
+	return 0;
+}
+
+int main(int argc, const char **argv)
 {
 	fprintf(stdout, "Hello from zwm!\n");
+
+	XSetErrorHandler(x_error_handler);
 
 	if (ZWM::WindowManager::the()->init() != 0)
 		return -1;
